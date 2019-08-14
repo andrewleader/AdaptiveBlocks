d73ee7f91fa25fd5882ce419ca30200a6966d398
Andrew Leader <aleader@microsoft.com>
2019-08-14 13:57:26 -0700

    Add WebNotification transformer

---
+++ AdaptiveBlocks.Transformers.WebNotification/AdaptiveBlockToWebNotificationTransformer.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaptiveBlocks.Transformers.WebNotification
+{
+    public class AdaptiveBlockToWebNotificationTransformer : AdaptiveBlockTransformer<WebNotification>
+    {
+        protected override async Task TransformAsync(AdaptiveBlock block, AdaptiveBlockTransformResult<WebNotification> result)
+        {
+            var content = block?.View?.Content;
+            if (content?.Title != null)
+            {
+                WebNotification notification = new WebNotification()
+                {
+                    Title = content.Title,
+                    Body = content.Subtitle,
+                    Image = content.Images?.FirstOrDefault()?.Url
+                };
+
+                foreach (var action in content.GetSimplifiedActions())
+                {
+                    if (action.Inputs.Count == 0 && action.Command != null)
+                    {
+                        notification.Actions.Add(new WebNotificationAction()
+                        {
+                            Title = action.Title,
+                            Action = JsonConvert.SerializeObject(action.Command)
+                        });
+                    }
+                }
+
+                result.Result = notification;
+            }
+            else
+            {
+                result.Errors.Add("There wasn't a title");
+            }
+        }
+    }
+}
---
+++ AdaptiveBlocks.Transformers.WebNotification/WebNotification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdaptiveBlocks.Transformers.WebNotification
+{
+    public class WebNotification
+    {
+        /// <summary>
+        /// The title of the notification
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The body string of the notification
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// The URL of an image to be displayed as part of the notification
+        /// </summary>
+        public string Image { get; set; }
+
+        public List<WebNotificationAction> Actions { get; } = new List<WebNotificationAction>();
+    }
+
+    public class WebNotificationAction
+    {
+        /// <summary>
+        /// The string describing the action that is displayed to the user.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The name of the action, which can be used to identify the clicked action similar to input names.
+        /// </summary>
+        public string Action { get; set; }
+    }
+}
